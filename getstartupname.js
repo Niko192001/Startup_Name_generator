7f81003e685ff69e5d6d9caf738f735c4124cad5
Niko192001 <nikolaj@rahe.dk>
2025-10-07 11:54:45 +0200

    Second commit but renamed with fixed solution

--- getstartupname.js
+++ getstartupname.js
@@ -1,2 +1,14 @@
-console.log('Happy developing ✨')
+
+const firstword = ['Tech', 'Inno', 'Smart', 'Blue', 'Next', 'Bright', 'Quantum', 'Rapid', 'Future', 'Digital'];
+const secondword = ['Vision', 'Solutions', 'Systems', 'Works', 'Dynamics', 'Hub', 'Network', 'Innovations', 'labs', 'path'];
+
+function getstartupname (a,b){
+    let randomNumberFromA = Math.floor(Math.random() * a.length);
+    let randomNumberFromB = Math.floor (Math.random()* b.length );
+    let randomElement = a[randomNumberFromA] + '' + b[randomNumberFromB];
+
+console.log(randomElement);
+}
+getstartupname(firstword, secondword);
+
 
